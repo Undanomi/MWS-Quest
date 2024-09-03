22ae303e95d33a866f63ad226a0ed9fe651f3de9
Almond-Latte <147462539+Almond-Latte@users.noreply.github.com>
2024-09-04 01:01:45 +0900

    #2 移動アニメーションの同期

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,17 @@ public class PlayerController : MonoBehaviour
     
     private float _horizontal;
     private float _vertical;
+
+    private Animator _anim;
+    private static readonly int LookX = Animator.StringToHash("Look X");
+    private static readonly int LookY = Animator.StringToHash("Look Y");
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,11 +27,33 @@ public class PlayerController : MonoBehaviour
     {
         _horizontal = Input.GetAxis("Horizontal");
         _vertical = Input.GetAxis("Vertical");
+        
+        // プレイヤーの向きを変更
+        SyncMoveAnimation();
     }
     
     void FixedUpdate()
     {
         Move();
+    }
+
+    /// <summary>
+    /// プレイヤーの向きと移動アニメーションの同期を行う
+    /// </summary>
+    private void SyncMoveAnimation()
+    {
+        Vector2 moveDirection = new Vector2(_horizontal, _vertical);
+        if (moveDirection.sqrMagnitude > 0)
+        {
+            _anim.SetFloat(LookX, moveDirection.x);
+            _anim.SetFloat(LookY, moveDirection.y);
+            _anim.SetBool(IsMoving, true);
+        }
+        else
+        {
+            _anim.SetBool(IsMoving, false);
+        }
+        
     }
     
     /// <summary>
