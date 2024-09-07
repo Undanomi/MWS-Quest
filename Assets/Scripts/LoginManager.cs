1ea30aaf96f6e4119eccbfea9efe7023394f1b8d
Almond-Latte <147462539+Almond-Latte@users.noreply.github.com>
2024-09-08 00:12:39 +0900

    仮ログイン処理の実装
    ユーザー名:admin
    パスワード:password
    で村のシーンへ遷移できるように実装

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LoginManager : MonoBehaviour
+{
+    [Header("ユーザ名入力フィールド")] public TMP_InputField usernameField;
+    [Header("パスワード入力フィールド")] public TMP_InputField passwordField;
+    [Header("ログインボタン")] public Button loginButton;
+    [Header("エラーメッセージ")] public TMP_Text errorMessage;
+    
+    public void OnLoginButtonClicked()
+    {
+        string username = usernameField.text;
+        string password = passwordField.text;
+        
+        // 本番では認証処理をサーバーにリクエストする
+        // StartCoroutine(LoginRequest(username, password));
+        
+        // 仮処理として、ユーザ名とパスワードが一致していればログイン成功とする
+        if (username == "admin" && password == "password")
+        {
+            // ログイン成功時には次のシーンに遷移する
+            SceneManager.LoadScene("Village");
+        }
+        else
+        {
+            // ログイン失敗時の処理
+            errorMessage.text = "ユーザ名またはパスワードが違います";
+        }
+        
+    }
+
+    private IEnumerator LoginRequest(string username, string password)
+    {
+        // こんな感じで本来はUnityWebRequestを使ってサーバーにリクエストを送る
+        // UnityWebRequest request = new UnityWebRequest("http://localhost:3000/login", "POST");
+        // ここでいろいろリクエスト内容を設定する
+        // ...
+        // リクエスト送信、レスポンス待ち
+        //yield return request.SendWebRequest();
+        
+        // レスポンス処理
+        // ...
+        
+        return null;
+    }
+}
