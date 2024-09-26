29ed0d6da724e752c8c77dc404f155d359d64a05
konekotech <89322889+konekotech@users.noreply.github.com>
2024-09-26 12:06:40 +0900

    add: information view object

---
+++ Assets/Scripts/InformationView.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class InformationView : MonoBehaviour
+{
+    public bool IsInformationViewAvailable { get; private set; }
+    public GameObject informationViewSystem;
+    public GameObject logAnalysisSystem;
+    public GameObject dialogueSystem;
+    
+    
+}
